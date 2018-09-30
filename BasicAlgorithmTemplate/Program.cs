ce726e58131fdf08576115a36c36dad56dc9fffc
Brian Jones <Brian.Jones@egov.com>
2018-09-29 21:10:53 -0600

    Add project files.

---
+++ BasicAlgorithmTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicAlgorithmTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
